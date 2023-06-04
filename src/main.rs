a2dc677db72e3a27f3465c6aa181f85c5f535624
Zack <zack@pop-os.localdomain>
2023-06-04 06:08:58 -0700

    Change the application structure for modularity and easier testing

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
-use actix_web::{get, App, HttpServer, HttpResponse, Responder};
-
-#[get("/health_check")]
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[tokio::main]
-async fn main() -> Result<(),std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .service(health_check)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+async fn main() -> Result<(), std::io::Error> {
+    run().await
 }
