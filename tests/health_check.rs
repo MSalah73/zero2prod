72d015f864f59f4a5c880e1036c8060d47624dd8
Zack <zack@pop-os.localdomain>
2023-06-05 19:44:01 -0700

    Added test cases for the subscription endpoint

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -18,6 +18,60 @@ async fn health_check_success() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    //Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=John73&email=john_r77%40gmail.com";
+
+    //Act
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to excute request");
+
+    //Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_when_data_is_missing() {
+    //Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = [
+        ("name=John73&email=","email is empty"),
+        ("name=John73&","email is missing"),
+        ("name=&email=john_r77%40gmail.com", "name is empty"),
+        ("email=john_r77%40gmail.com","name is missing"),
+        ("name=&email=", "both email and name are empty"),
+        ("", "both email and name are missing")
+    ];
+
+    //Act
+    for (invalid_body, error_message) in test_cases { 
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to excute request");
+
+        //Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with status code 400 when the payload was {}",
+            error_message
+        );
+    }
+}
+
 fn spawn_app() -> String {
     //Retrieve random port assigned by the OS
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
