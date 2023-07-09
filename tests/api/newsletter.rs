bd3dbd866af9880a1dffc1c8ff766d32d0736a92
Zack <zack@pop-os.localdomain>
2023-07-09 00:05:46 -0700

    Added happy path test for newsletter

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,4 +1,6 @@
-use crate::helper::{spawn_app, TestApp};
+use crate::helper::{spawn_app, ConfirmationLinks, TestApp};
+use actix_web::HttpResponse;
+use reqwest::Request;
 use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
 
@@ -7,7 +9,7 @@ async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
     // Arrange
     let app = spawn_app().await;
 
-    create_unconfirmed_subscribers(&app).await;
+    let _ = create_unconfirmed_subscriber(&app).await;
 
     Mock::given(any())
         .respond_with(ResponseTemplate::new(200))
@@ -36,7 +38,40 @@ async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
     //Mock verifies on Drop that we haven't send any newsletters
 }
 
-async fn create_unconfirmed_subscribers(app: &TestApp) {
+#[tokio::test]
+async fn newsletters_are_delivered_to_confirmed_subscribers() {
+    // Arrange
+    let app = spawn_app().await;
+
+    create_confirmed_subscriber(&app).await;
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    let newsletter_body_request = serde_json::json!({
+        "title": "Newsletter Title",
+        "content": {
+            "html":"<p>Newsletter body as HTML</P>",
+            "text":"Newsletter body as plain text",
+        },
+    });
+
+    // Act
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&newsletter_body_request)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Asserrt
+    assert_eq!(response.status().as_u16(), 200);
+}
+async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
     let body = "name=John73&email=john_r77%40gmail.com";
 
     // We need to use _veriable_name for the guard to be droped at the end of the scope
@@ -51,6 +86,25 @@ async fn create_unconfirmed_subscribers(app: &TestApp) {
 
     app.post_subscriptions(body.into())
         .await
+        .error_for_status()
+        .unwrap();
+
+    let email_request = &app
+        .email_server
+        .received_requests()
+        .await
+        .unwrap()
+        .pop()
+        .unwrap();
+
+    app.get_confirmation_links(&email_request)
+}
+
+async fn create_confirmed_subscriber(app: &TestApp) {
+    let confirmation_link = create_unconfirmed_subscriber(app).await;
+    reqwest::get(confirmation_link.html)
+        .await
+        .unwrap()
         .error_for_status()
         .unwrap();
 }
