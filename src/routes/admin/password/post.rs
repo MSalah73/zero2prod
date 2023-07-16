16fe98d79a5b2ee7320d196d77987b75b201fbba
Zack <zack@pop-os.localdomain>
2023-07-16 10:27:28 -0700

    Added flash error to change password form

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,3 +1,5 @@
+use crate::authentication::{validate_credentials, AuthError, Credentials};
+use crate::routes::admin::dashboard::get_username;
 use crate::session_state::TypedSession;
 use crate::utils::{e500, see_other};
 use actix_web::{post, web, HttpResponse};
@@ -8,25 +10,51 @@ use sqlx::PgPool;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
-    _current_password: Secret<String>,
+    current_password: Secret<String>,
     new_password: Secret<String>,
     check_new_password: Secret<String>,
 }
 
-#[tracing::instrument(name = "Change password", skip(form, session, _pool))]
+#[tracing::instrument(name = "Change password", skip(form, session, pool))]
 #[post("/admin/password")]
 pub async fn change_password(
     form: web::Form<FormData>,
     session: TypedSession,
-    _pool: web::Data<PgPool>,
+    pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
     if session.get_user_id().map_err(e500)?.is_none() {
         return Ok(see_other("/login"));
     }
 
+    let user_id = session.get_user_id().map_err(e500)?;
+
+    if user_id.is_none() {
+        return Ok(see_other("/login"));
+    }
+
+    let user_id = user_id.unwrap();
+
     if form.new_password.expose_secret() != form.check_new_password.expose_secret() {
         FlashMessage::error("New password entries does not match.").send();
         return Ok(see_other("/admin/password"));
     }
+
+    let username = get_username(user_id, &pool).await.map_err(e500)?;
+
+    let credentials = Credentials {
+        username,
+        password: form.0.current_password,
+    };
+
+    if let Err(e) = validate_credentials(credentials, &pool).await {
+        return match e {
+            AuthError::InvalidCredentials(_) => {
+                FlashMessage::error("The current password is incorrect.").send();
+                Ok(see_other("/admin/password"))
+            }
+            AuthError::UnexpectedError(_) => Err(e500(e)),
+        };
+    }
+
     Ok(HttpResponse::Ok().finish())
 }
--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -27,6 +27,7 @@ async fn user_must_be_logged_in_to_change_the_password() {
     // Assert
     assert_is_redirect_to(&response, "/login")
 }
+
 #[tokio::test]
 async fn new_password_fields_must_match() {
     // Arrange
@@ -55,4 +56,34 @@ async fn new_password_fields_must_match() {
     // Act 2 -- Follow the redirect
     let html_password_form = app.get_change_password_html().await;
     assert!(html_password_form.contains("<p><i>New password entries does not match.</i></p>"));
+}
+
+#[tokio::test]
+async fn current_password_must_be_valid() {
+    // Arrange
+    let app = spawn_app().await;
+
+    let login_body = serde_json::json!({
+    "username": app.test_user.username,
+    "password": app.test_user.password,
+    });
+
+    let new_password = Uuid::new_v4().to_string();
+    let wrong_current_password = Uuid::new_v4().to_string();
+    let change_password_body = serde_json::json!({
+        "current_password" : &wrong_current_password,
+        "new_password" : &new_password,
+        "check_new_password" : &new_password,
+    });
+
+    // Act 1 -- Login and attempt to change password
+    app.post_login(&login_body).await;
+
+    let response = app.post_change_password(&change_password_body).await;
+
+    // Assert 1
+    assert_is_redirect_to(&response, "/admin/password");
+    // Act 2 -- Follow the redirect
+    let html_password_form = app.get_change_password_html().await;
+    assert!(html_password_form.contains("<p><i>The current password is incorrect.</i></p>"));
 }
